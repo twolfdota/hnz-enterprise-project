8514eb9360ccf3046b297c51e76948d82320e10c
tuan anh dao ngoc <twolfdota@gmail.com>
2019-03-01 14:05:57 +0700

    Update hello.html
    thêm form create user và form sửa thông tin deadlines

---
+++ Project/js/style.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+     $('.slider-nav').slick({
+   slidesToShow: 1,
+   slidesToScroll: 2,
+   autoplay: true,
+   dots: false,
+   focusOnSelect: true
+ });
+});
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > 100) {
+        $('#back2Top').fadeIn();
+    } else {
+        $('#back2Top').fadeOut();
+    }
+});
+$(document).ready(function() {
+    $("#back2Top").click(function(event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });
+
+});
+
+ $(document).ready(function() ){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
