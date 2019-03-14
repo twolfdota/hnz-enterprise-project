6cdb5552175828ea2a29ff76b1617f4498c20828
tuan anh dao ngoc <twolfdota@gmail.com>
2019-03-14 14:22:51 +0700

    Update home.php

--- views/home.php
+++ views/home.php
@@ -165,7 +165,35 @@
     </header>
     <content>
         <div class="container">
+					<?php
+            $ch = curl_init();
+            $file_name_with_full_path = "/xampp/htdocs/hnz-enterprise-project/uploads/test/guide.doc";
 
+            curl_setopt($ch, CURLOPT_URL, "https://api.docconversionapi.com/convert?outputFormat=html");
+            curl_setopt($ch, CURLOPT_POST, 2);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-ApplicationID:286f4243-5ed4-450d-bce9-435f568d450b', 'X-SecretKey:120748dd-ac15-44fd-88c8-ca46149b4b8d'));
+
+            if (function_exists('curl_file_create')) { // php 5.5+
+              $cFile = curl_file_create($file_name_with_full_path, 'application/msword', basename($file_name_with_full_path));
+            } else { //
+              $cFile = '@' . realpath($file_name_with_full_path);
+            }
+
+            $post = array(
+              "optionsJson" => "{}",
+              "inputFile" => $cFile
+            );
+
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            $server_output = curl_exec($ch);
+
+            if ($server_output === false) {
+              echo 'Curl error occurred: ' . curl_error($ch);
+            } else {
+              echo $server_output;
+            }
+            ?>
         </div>
 
     </content>
