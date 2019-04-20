71811e0e7fee46ef122abb5f3c7d0211135317de
vuvantien1997hd <48012883+vuvantien1997hg@users.noreply.github.com>
2019-04-21 00:35:27 +0700

    up

--- views/postdetail.php
+++ views/postdetail.php
@@ -167,24 +167,61 @@
                                 <p>Màu xanh dương, màu của bầu trời, màu của biển luôn là chủ đề yêu thích cho mùa hè. Có rất nhiều cách để mang phong cách và chủ đề này vào phòng ngủ của trẻ. Từ những đồ nội thất mang hình dáng những con tàu biển tới bức tường được trang trí với lưới đánh cá, có rất nhiều ý tưởng sáng tạo cho bạn lựa chọn.</p>
                                 <img src="https://e2.365dm.com/13/12/768x432/Manchester-United-v-Newcastle-Red-DEvils-deje_3047907.jpg?20131209162127">
                             </div>
-                            <div class="commentPost">
-                                <div class="row">
-                                    <div class="imgComent col-lg-2 col-md-3 col-sm-2 col-xs-3">
-                                        <img src="https://i0.wp.com/www.winhelponline.com/blog/wp-content/uploads/2017/12/user.png?fit=256%2C256&quality=100&ssl=1">
-                                    </div>
-                                    <div class="inputComent col-lg-8 col-md-7 col-sm-7 col-xs-9">
-                                        <div class="form-group">
-                                            <textarea placeholder="Add comment..." class="form-control" rows="5" id="Notes"></textarea>
+                            <div class="title">
+                                <h4>Đánh giá & Nhận xét</h4>
+                            </div>
+                            <div class="commentEveryone">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="oneComment">
+                                        <div class="img">
+                                            <img src="https://scontent.fhan3-3.fna.fbcdn.net/v/t1.0-9/47470959_496964537461125_7089170012576940032_n.jpg?_nc_cat=103&_nc_oc=AQmkkgk2xglrPDxKfTb7GCdMVZw8jOlmwOCFVUlABnFjPbOkF7MZXYhQvabGh2W5W6U&_nc_ht=scontent.fhan3-3.fna&oh=c6fd84c9dc7bff64bfca5b860da1409d&oe=5D75396D">
+                                        </div>
+                                        <div class="content">
+                                            <p>
+                                                Màu xanh dương, màu của bầu trời, màu của biển luôn là chủ đề yêu thích cho mùa hè. Có rất nhiều cách để mang phong cách và chủ đề này vào phòng ngủ của trẻ. Từ những đồ nội thất mang hình dáng những con tàu biển tới bức tường được trang trí với lưới đánh cá, có rất nhiều ý tưởng sáng tạo cho bạn lựa
+                                            </p>
+                                            <p class="text-right">
+                                                <span>VuVanTien <time>20/4/2019</time>
+                                                </span>
+                                            </p>
                                         </div>
                                     </div>
-                                    <div class="btnComent col-lg-2 col-md-2 col-sm-3 col-xs-12">
-                                        <input type="submit" name="" value="Add Comment">
+                                    <div class="oneComment">
+                                        <div class="img">
+                                            <img src="https://scontent.fhan3-3.fna.fbcdn.net/v/t1.0-9/47470959_496964537461125_7089170012576940032_n.jpg?_nc_cat=103&_nc_oc=AQmkkgk2xglrPDxKfTb7GCdMVZw8jOlmwOCFVUlABnFjPbOkF7MZXYhQvabGh2W5W6U&_nc_ht=scontent.fhan3-3.fna&oh=c6fd84c9dc7bff64bfca5b860da1409d&oe=5D75396D">
+                                        </div>
+                                        <div class="content">
+                                            <p>
+                                                Màu xanh dương, màu của bầu trời, màu của biển luôn là chủ đề yêu thích cho mùa hè. Có rất nhiều cách để mang phong cách và chủ đề này vào phòng ngủ của trẻ. Từ những đồ nội thất mang hình dáng những con tàu biển tới bức tường được trang trí với lưới đánh cá, có rất nhiều ý tưởng sáng tạo cho bạn lựa
+                                            </p>
+                                            <p class="text-right">
+                                                <span>VuVanTien <time>20/4/2019</time>
+                                                </span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="title">
+                                <h4>Your Comment</h4>
+                            </div>
+                            <div class="commentPost">
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="row">
+                                        <div class="inputComent col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <div class="form-group">
+                                                <textarea placeholder="Add comment..." class="form-control" rows="5" id="Notes"></textarea>
+                                            </div>
+                                        </div>
+                                        <div class="btnComent col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                            <input type="submit" name="" value="Add Comment">
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="col-lg-3 col-md-3 hidden-xs hidden-sm">
-                            
+                            <h3>Tin tức mới nhất</h3>
                         </div>
                     </div>
                 </div>
